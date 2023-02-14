c12522d4a12c9067f89601e0f0977ecfbe14ac9a
Irirwanirira <irijoseph41@gmail.com>
2023-02-14 12:50:43 +0200

    Set database

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  resources :add_user_to_recipes
+  resources :recipes
+  resources :add_recipe_to_recipe_foods
+  resources :add_food_to_recipe_foods
+  resources :recipe_foods
+  resources :foods
+  resources :add_user_to_inventories
+  resources :add_inventory_to_inventory_foods
+  resources :add_food_to_inventory_foods
+  resources :inventory_foods
+  resources :inventories
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
