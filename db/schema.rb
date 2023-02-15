ebc8ec00bedf7c477302d49ecf8a66eaa7e1725d
Irirwanirira <irijoseph41@gmail.com>
2023-02-15 11:56:36 +0200

    Resolve conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_02_14_192802) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-
-ActiveRecord::Schema[7.0].define(version: 2023_02_14_130917) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_14_192454) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -86,6 +67,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_130917) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "inventories", "users"
@@ -94,5 +82,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_130917) do
   add_foreign_key "recipe_foods", "foods"
   add_foreign_key "recipe_foods", "recipes"
   add_foreign_key "recipes", "users"
-
 end
