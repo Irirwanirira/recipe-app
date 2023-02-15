3221364757dc45169c043477e55563c83375bc7a
Irirwanirira <irijoseph41@gmail.com>
2023-02-15 10:04:57 +0200

    Added user's through authentication

--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_14_192802) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "surname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
